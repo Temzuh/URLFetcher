4cbc920c5250156668089043ca48c415c33f44b4
Temzuh <teme.kukkonen@gmail.com>
2022-02-02 09:56:59 +0200

    Finishing binary fetch

--- URLFetcherandparsing.py
+++ URLFetcherandparsing.py
@@ -4,34 +4,38 @@ from urllib.request import urlopen
 import urllib.request
 
 
-
 page = input("Give url: ")
 try:
-    with urlopen(page) as f:
-        data = f.read()
-        s = data.decode("utf-8")
-        y = len(re.findall(r"\b(bomb|murder|kill|terrorism|terror|terrorists|terrorist)\b", s))
-        print("The number of bad words: ", str(y))
-
-        x = input("give name for the file: ")
-        if x:
-            print(x)
-        else:
-            print("Saving failed.")
-        urllib.request.urlretrieve(page, x)
-
+    with urlopen(page) as resp:
+        data = resp.read()
+        bad_word_count = 0
+        try:
+            decoded = data.decode('utf-8')
+            bad_word_count = len(re.findall(r"\b(bomb|murder|kill|terrorism|terror|terrorists|terrorist)\b", decoded))
+            print("The number of bad words: ", bad_word_count)
+        except TypeError:
+            print("Downloading binary file...")
+        except UnicodeDecodeError:
+            print("Downloading binary file...")
+        finally: 
+            x = input("give name for the file: ")
+            if x:
+                print(x)
+                urllib.request.urlretrieve(page, x)
+            else:
+                print("Saving failed.")
         
-except ValueError:
-    print("", page)
+except ValueError as e:
+    print("error", page, e)
 
-except HTTPError:
-    print("HTTP ERROR", page)
+except HTTPError as e:
+    print("HTTP ERROR", page, e)
 
-except OSError:
-    print("Error opening URL!", page)
+except OSError as e:
+    print("Error opening URL!", page, e)
 
-except TypeError:
-    print("Saving failed", page)
+except TypeError as e:
+    print("Saving failed", page, e)
 
 #JPG https://upload.wikimedia.org/wikipedia/commons/0/07/Bomban_talo_1.jpg
 #http://brokenurl.doesntexist.com/
